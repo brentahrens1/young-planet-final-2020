f13861c0af9333c6345eb446d84c237530d062f4
Brent Ahrens <cidida@gmail.com>
2020-07-27 07:21:29 -0700

    creates landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react'
+import '../../sass/_landing-page.scss'
 
 const LandingPage = () => {
     return (
         <div className="landing">
-
+            <div className="close">
+                <div className="close__bar" />
+                <div className="close__bar" />
+            </div>
+            <div className="landing__content">
+                <h1>This is the landing page</h1>
+            </div>
         </div>
     )
 }
