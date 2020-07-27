35eb34e462e5d6758250e6abe53f1364892c8556
Brent Ahrens <cidida@gmail.com>
2020-07-27 07:06:52 -0700

    finishing touches on nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 
 import NavBar from './components/NavBar/NavBar'
+import LandingPage from './components/LandingPage/LandingPage'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <h1>This is the app</h1>
+      <LandingPage />
     </div>
   );
 }
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const LandingPage = () => {
+    return (
+        <div className="landing">
+
+        </div>
+    )
+}
+
+export default LandingPage
