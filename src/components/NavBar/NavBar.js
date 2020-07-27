c40bcee2cf9d829998de6d1710cfdd73bb85a976
Brent Ahrens <cidida@gmail.com>
2020-07-27 03:44:56 -0700

    young planet site set up

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const NavBar = () => {
+    return(
+        <div>
+            <h1>This is the nav</h1>
+        </div>
+    )
+}
+
+export default NavBar
