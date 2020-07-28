328366ae2aefceb278d4c9f6bc6173c7b9b3afd2
Brent Ahrens <cidida@gmail.com>
2020-07-28 07:30:50 -0700

    responsiveness for static sections

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -7,8 +7,8 @@ import Orare from '../../images/covers/orare.jpg'
 const Homepage = () => {
     return (
         <div className="home">
-            <div className="home__section home__parallax home__banner1" />
-            <div className="home__section home__static">
+            <div className="home__section home__parallax home__banner1"></div>
+            <div className="home__static">
                 <div class="home__static-image">
                     <img src={Orare} alt="Young Planet x Downtown Dawson - Orare" />
                 </div>
@@ -18,7 +18,7 @@ const Homepage = () => {
                     <h2><a href="#">Listen Now</a></h2>
                 </div>
             </div>
-            <div className="home__section home__parallax home__banner2" />
+            <div className="home__section home__parallax home__banner2"></div>
             <div className="home__video">
                 <div class="video-container video-one">
                     <iframe src="https://www.youtube.com/embed/QdxfZIarrC0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -27,7 +27,7 @@ const Homepage = () => {
                     <iframe src="https://www.youtube.com/embed/7wwhUPkcJ3U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
-            <div className="home__section home__parallax home__banner3" />
+            <div className="home__section home__parallax home__banner3"></div>
         </div>
     )
 }
