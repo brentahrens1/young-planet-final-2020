b64b9e75fc1b65e431853fab833365215d651903
brentahrens1 <cidida@gmail.com>
2020-07-27 17:45:57 -0700

    homepage component edits

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import '../../sass/_homepage.scss'
 
+
+import Orare from '../../images/covers/orare.jpg'
+
 const Homepage = () => {
     return (
         <div className="home">
             <div className="home__section home__parallax home__banner1" />
             <div className="home__section home__static">
-                <h1>Section one</h1>
+                <div class="home__static-image">
+                    <img src={Orare} alt="Young Planet x Downtown Dawson - Orare" />
+                </div>
+                <div className="home__static-content">
+                    <h2>Latest Release</h2>
+                    <h4>Available now on all platforms for stream / download</h4>
+                    <button>Listen Now</button>
+                </div>
             </div>
             <div className="home__section home__parallax home__banner2" />
             <div className="home__static">
